2c0de6f9be02e76547511a636cd450ed506040de
ChidiBrokeDNet <59733962+devevangel@users.noreply.github.com>
2023-11-27 03:00:45 +0000

    Finished coding requriments

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-const express = require('express')
-const path = require('path')
-const stocks = require('./stocks')
+const express = require('express');
+const path = require('path');
+const stocks = require('./stocks');
 
-const app = express()
-app.use(express.static(path.join(__dirname, 'static')))
+const app = express();
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/stocks', async (req, res) => {
-  const stockSymbols = await stocks.getStocks()
-  res.send({ stockSymbols })
-})
+  try {
+    const stockSymbols = await stocks.getStocks();
+    res.status(200).send({ stockSymbols });
+  } catch (error) {
+    console.log('unable to provide stock data');
+    res.status(400).send('unable to provide stock data');
+  }
+});
 
 app.get('/stocks/:symbol', async (req, res) => {
-  const { params: { symbol } } = req
-  const data = await stocks.getStockPoints(symbol, new Date())
-  res.send(data)
-})
+  const {
+    params: { symbol },
+  } = req;
+  try {
+    const data = await stocks.getStockPoints(symbol, new Date());
+    res.status(200).send(data);
+  } catch (error) {
+    console.log(`unable to provide stock data for '${symbol}'`);
+    res.status(400).send(`unable to provide stock data for '${symbol}'`);
+  }
+});
 
-app.listen(3000, () => console.log('Server is running!'))
+app.listen(3000, () => console.log('Server is running!'));
--- static/scripts.js
+++ static/scripts.js
@@ -1,24 +1,101 @@
-const canvas = document.getElementById('chart')
-const ctx = canvas.getContext('2d')
+const loadingEl = document.querySelector('.spinner-container');
 
-function drawLine (start, end, style) {
-  ctx.beginPath()
-  ctx.strokeStyle = style || 'black'
-  ctx.moveTo(...start)
-  ctx.lineTo(...end)
-  ctx.stroke()
+const canvas = document.getElementById('chart');
+const ctx = canvas.getContext('2d');
+
+function drawLine(start, end, style) {
+  ctx.beginPath();
+  ctx.strokeStyle = style || 'black';
+  ctx.moveTo(...start);
+  ctx.lineTo(...end);
+  ctx.stroke();
 }
 
-function drawTriangle (apex1, apex2, apex3) {
-  ctx.beginPath()
-  ctx.moveTo(...apex1)
-  ctx.lineTo(...apex2)
-  ctx.lineTo(...apex3)
-  ctx.fill()
+function drawTriangle(apex1, apex2, apex3) {
+  ctx.beginPath();
+  ctx.moveTo(...apex1);
+  ctx.lineTo(...apex2);
+  ctx.lineTo(...apex3);
+  ctx.fill();
 }
 
-drawLine([50, 50], [50, 550])
-drawTriangle([35, 50], [65, 50], [50, 35])
+// Handles showing the loading animation
+function showLoad() {
+  loadingEl.classList.remove('hide');
+  loadingEl.classList.add('load');
+}
+// Handles hiding the loading animation
+function hideLoad() {
+  loadingEl.classList.remove('load');
+  loadingEl.classList.add('hide');
+}
 
-drawLine([50, 550], [950, 550])
-drawTriangle([950, 535], [950, 565], [965, 550])
+// Calls hide and show animation function based on state input
+function handlingLoading(state) {
+  switch (state) {
+    case 'START':
+      showLoad();
+      break;
+    case 'END':
+      hideLoad();
+      break;
+
+    default:
+      hideLoad();
+      break;
+  }
+}
+
+// Gets all avaliable stock data (returns an array)
+async function loadAvailableStocks() {
+  handlingLoading('START');
+  const response = await fetch('/stocks');
+  const data = response.json();
+  return data;
+}
+
+// Gets data about specific stock symbol
+async function getSymbolStockData(name) {
+  const response = await fetch(`/stocks/${name}`);
+  const data = response.json();
+  return response.status === 200 ? data : [];
+}
+
+// Handles all server or API request function calls and returns data in a key value pair frmart
+async function loadAllData() {
+  let stockData = {};
+  try {
+    const { stockSymbols } = await loadAvailableStocks();
+    if (stockSymbols) {
+      for (const symbol of stockSymbols) {
+        const result = await getSymbolStockData(symbol);
+        stockData = {
+          ...stockData,
+          [symbol]: result,
+        };
+      }
+    }
+
+    handlingLoading('END');
+  } catch (error) {
+    handlingLoading('END');
+    console.log('An error occured fetching stock data.', error);
+  }
+
+  return stockData;
+}
+
+// On page load
+async function onPageLoad() {
+  const data = await loadAllData();
+
+  console.log(data);
+
+  drawLine([50, 50], [50, 550]);
+  drawTriangle([35, 50], [65, 50], [50, 35]);
+
+  drawLine([50, 550], [950, 550]);
+  drawTriangle([950, 535], [950, 565], [965, 550]);
+}
+
+window.addEventListener('load', onPageLoad);
